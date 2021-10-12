38f150089e9599b2f88e7f124d86f8d2b91e98a2
MacdonaldRobinson <maocnly2001@gmail.com>
2021-10-12 07:54:29 -0600

    removed unneeded addons and created new exports

---
+++ git-clean.bat
@@ -0,0 +1,5 @@
+git filter-branch --tree-filter 'rm -rf worlds/terrains' HEAD
+git filter-branch --tree-filter 'rm -rf addons' HEAD
+git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
+git reflog expire --expire=now --all
+git gc --prune=now
