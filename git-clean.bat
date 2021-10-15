1f15225820b60ad9fb94fb8d8b588c27c080d031
MacdonaldRobinson <maocnly2001@gmail.com>
2021-10-14 18:01:20 -0600

    removed cleanup script from git

--- git-clean.bat
+++
@@ -1,6 +0,0 @@
-git filter-branch --tree-filter 'rm -rf worlds/terrains' HEAD
-git filter-branch --tree-filter 'rm -rf addons' HEAD
-git filter-branch --tree-filter 'rm -rf assets' HEAD
-git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
-git reflog expire --expire=now --all
-git gc --prune=now
