6889d7699f0d804346277dc83dcc8062315fb873
MacdonaldRobinson <maocnly2001@gmail.com>
2021-10-15 18:34:40 -0600

    fixed missing player and fixed issues

--- git-clean.bat
+++ git-clean.bat
@@ -4,3 +4,6 @@ git filter-branch -f --tree-filter 'rm -rf assets' HEAD
 git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
 git reflog expire --expire=now --all
 git gc --prune=now
+
+#git push origin --force --all
+#git push origin --force --tags
